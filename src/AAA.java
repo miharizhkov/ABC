f715489b4c305d6c7b4e7f3500c32972c580d8cb
михаил <miharizhkov@gmail.com>
2022-07-16 13:11:02 +0500

    2 задача 2 уровня

--- src/AAA.java
+++ src/AAA.java
@@ -6,23 +6,27 @@ public class AAA {
         String world = " WoRld!";
         char newLine = '\n';
         String helloWorld = hi + world + newLine;
-        String A = (hi.trim()+world.toLowerCase());
-        System.out.println(A+newLine+A+newLine+A);
+        String o = (hi.trim()+world.toLowerCase());
+        System.out.println(o+newLine+o+newLine+o);
         // Задача 2
         double rost = 1.85;
         double ves = 85.3;
         double imt = ves / (rost * rost);
         System.out.println(imt);
         // Задача 3
-        char[] a = {'a','b', 'c','d', 'e'};
-        System.out.println(a);
-        a[3] = 'h';
-        System.out.println(a);
+        char[] p = {'a','b', 'c','d', 'e'};
+        System.out.println(p);
+        p[3] = 'h';
+        System.out.println(p);
         // Уровень 2
         // Задача 1
         String i = "234";
         int v = Integer.parseInt(i.trim());
         System.out.println(v+"some_text".length());
         // Задача 2
+        double a = 3;
+        double b = 5;
+        double c = Math.pow((a+b), 2);
+        System.out.println(c);
     }
 }
